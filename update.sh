1eccd7d8602bac79679cd6028eed84849c1a1f4d
Tim Montague <tmontague@atinc.com>
2012-01-30 10:42:33 -0500

    added vim-snipmate and removed undofile (which wasn't being used)

--- update.sh
+++ update.sh
@@ -4,7 +4,11 @@ REPOS="https://github.com/timmontague/jekyll.vim.git
 https://github.com/plasticboy/vim-markdown.git
 https://github.com/vim-scripts/closetag.vim.git
 https://github.com/majutsushi/tagbar.git
-https://github.com/vim-scripts/undofile.vim.git"
+https://github.com/garbas/vim-snipmate.git
+https://github.com/MarcWeber/vim-addon-mw-utils.git
+https://github.com/tomtom/tlib_vim.git"
+
+# vim-addon-mw-utils and tlib_vim are requirements of vim-snipmate
 
 if [ ! -d bundle ]
 then
